172606291fbc702c5b3c5612c8252f0fe436aba0
TheWebcode <thewebcode@thewebcode.eu>
2023-01-02 22:57:57 +0100

    Added Eventlistener for Menues

---
+++ YCore/src/main/java/io/github/thewebcode/ycore/event/Eventlistener.java
@@ -0,0 +1,25 @@
+package io.github.thewebcode.ycore.event;
+
+import io.github.thewebcode.ycore.menu.Menu;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.InventoryHolder;
+
+public class Eventlistener implements Listener {
+    @EventHandler
+    public void onMenuClick(InventoryClickEvent e){
+        if(e.getCurrentItem() == null) return;
+        if(e.getCurrentItem().getItemMeta() == null) return;
+
+        InventoryHolder holder = e.getInventory().getHolder();
+        if (holder instanceof Menu) {
+            e.setCancelled(true);
+            if (e.getCurrentItem() == null) {
+                return;
+            }
+            Menu menu = (Menu) holder;
+            menu.handleMenu(e);
+        }
+    }
+}
